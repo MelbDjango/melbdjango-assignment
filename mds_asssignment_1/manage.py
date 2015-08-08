a3d782b90eb428df462af49eab029339b96c0b91
Moss Banay <mossbanay@gmail.com>
2015-08-08 11:51:56 +1000

    Renamed project
    Renamed project after realising guestbook should be an app

--- mds_asssignment_1/manage.py
+++ mds_asssignment_1/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "guestbook.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mds_asssignment_1.settings")
 
     from django.core.management import execute_from_command_line
 
--- mds_asssignment_1/mds_asssignment_1/urls.py
+++ mds_asssignment_1/mds_asssignment_1/urls.py
@@ -1,4 +1,4 @@
-"""guestbook URL Configuration
+"""mds_asssignment_1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
