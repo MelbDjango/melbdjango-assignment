5f2e5d47b4b5d0fa03526e90b1227a1116ee6696
Moss Banay <mossbanay@gmail.com>
2015-08-08 21:55:46 +1000

    Small tweaks in form
    Added in thank-you message, fixed post count to use the correct form
    (post/posts), hid entry form when a user POSTs a successful comment.
    Also tweaked model to remove unneeded code in the home view when saving
    an entry.

--- mds_asssignment_1/guestbook/models.py
+++ mds_asssignment_1/guestbook/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-import datetime
+from django.utils import timezone
 
 class Entry(models.Model):
     fname = models.CharField(max_length=50, verbose_name='First Name')
-    lname = models.CharField(max_length=50, verbose_name='Last Name')
-    email = models.CharField(max_length=100)
+    lname = models.CharField(max_length=50, verbose_name='Last Name', blank=True)
+    email = models.CharField(max_length=100, blank=True)
     comment = models.CharField(max_length=500)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
     visible = models.BooleanField(default=True)
 
     def __str__(self):
--- mds_asssignment_1/guestbook/views.py
+++ mds_asssignment_1/guestbook/views.py
@@ -3,27 +3,26 @@ from django.shortcuts import render
 from .forms import EntryForm
 from .models import Entry
 
-import datetime
-
 
 def home(request):
+    entry_list = Entry.objects.all().filter(visible=True).order_by('date').reverse()
     if request.method == 'POST':
         entry_form = EntryForm(request.POST)
         if entry_form.is_valid():
-            # do something
             entry = Entry()
             entry.fname = entry_form.cleaned_data['fname']
             entry.lname = entry_form.cleaned_data['lname']
             entry.email = entry_form.cleaned_data['email']
             entry.comment = entry_form.cleaned_data['comment']
-            entry.visible = True
-            entry.date = datetime.datetime.now()
             entry.save()
+            return render(request, 'entries.html', {
+                'thankyou': entry.fname,
+                'entry_list': entry_list,
+            })
 
     else:
         entry_form = EntryForm()
 
-    entry_list = Entry.objects.all().filter(visible=True).order_by('date').reverse()
     return render(request, 'entries.html', {
         'entry_list': entry_list,
         'entry_form': entry_form,
