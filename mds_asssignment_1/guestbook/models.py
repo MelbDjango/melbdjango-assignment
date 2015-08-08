b9de4445cef0ce6d1ad6b65004aa03fdbae61e5c
Moss Banay <mossbanay@gmail.com>
2015-08-08 14:47:14 +1000

    Restyled the webpage
    Added text displaying how old each post is, ensured the ordering of the
    posts is such that the newest post is at the top of the page.

--- mds_asssignment_1/guestbook/models.py
+++ mds_asssignment_1/guestbook/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 class Entry(models.Model):
     fname = models.CharField(max_length=50, verbose_name='First Name')
@@ -10,3 +11,11 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.comment
+
+    def age(self):
+        # Returns a string with how old the entry is
+        td = datetime.datetime.now(datetime.timezone.utc) - self.date
+        s = td.total_seconds()
+        hours, remainder = divmod(s, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return '{} hours, {} minutes, {} seconds ago.'.format(*map(int, (hours, minutes, seconds)))
--- mds_asssignment_1/guestbook/views.py
+++ mds_asssignment_1/guestbook/views.py
@@ -6,7 +6,7 @@ def home(request):
     return entries(request)
 
 def entries(request):
-    entry_list = Entry.objects.all()
+    entry_list = Entry.objects.all().order_by('date').reverse()
     return render(request, 'entries.html', {
         'entry_list': entry_list,
     })
