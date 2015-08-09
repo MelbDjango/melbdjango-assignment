6d274db33bd6b11e50f56d086d5d3e47ad480f8d
Moss Banay <mossbanay@gmail.com>
2015-08-09 12:28:04 +1000

    Change Entry model comment to TextField
    Oops, forgot to do this in previous commit

--- mds_asssignment_1/guestbook/models.py
+++ mds_asssignment_1/guestbook/models.py
@@ -5,7 +5,7 @@ class Entry(models.Model):
     fname = models.CharField(max_length=50, verbose_name='First Name')
     lname = models.CharField(max_length=50, verbose_name='Last Name', blank=True)
     email = models.CharField(max_length=100, blank=True)
-    comment = models.CharField(max_length=500)
+    comment = models.TextField(max_length=500)
     date = models.DateTimeField(default=timezone.now)
     visible = models.BooleanField(default=True)
 
