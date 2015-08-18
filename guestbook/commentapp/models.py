3756bbcaf903256b5d638bcb71838a20e8427f28
Andrew Artajos <andrew.artajos.dev@gmail.com>
2015-08-18 23:33:54 +1000

    initial draft of melbdjango guestbook TODO: CSS to make it handsome and tests

---
+++ guestbook/commentapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class GuestPost(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(blank=True)
+    show = models.BooleanField(default=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return '{} [{}] - {} {}'.format(self.name, self.email, self.pub_date, self.comment)
+
+    def is_visible(self):
+        return self.show
