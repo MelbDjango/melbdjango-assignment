42b3201ed0ce0326375704cb61197abe6e59733c
cxhandley <handley.craig@gmail.com>
2015-08-18 22:40:40 +1000

    Added model visualization and begin making Factories for testing

--- guestbook/guestbook/settings.py
+++ guestbook/guestbook/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'foundation',
     'register',
     'comments',
@@ -110,3 +111,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.abspath(BASE_DIR + '/static/'),
 )
+
+GRAPH_MODELS = {
+    'all_applictions': True,
+    'group_models': True,
+}
