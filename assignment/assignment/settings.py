68b47c4b6af9d0c3d700ea2f40e9d9f810004b87
Karl Urdevics <karl@kvurd.com>
2015-08-20 02:42:56 +1000

    added index page, visual tweaks, pagination

--- assignment/assignment/settings.py
+++ assignment/assignment/settings.py
@@ -105,3 +105,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
+
+# for make benefit bootstrap 3
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
--- assignment/guestbook/views.py
+++ assignment/guestbook/views.py
@@ -1,11 +1,13 @@
 from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.urlresolvers import reverse_lazy
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 from .forms import PostForm
 
 
-class PostCreateList(CreateView):
+class PostCreateList(SuccessMessageMixin, CreateView):
     """
     A view to create and list posts.
 
@@ -16,13 +18,23 @@ class PostCreateList(CreateView):
     model = Post
     template_name = 'guestbook/guestbook.html'
     success_url = reverse_lazy('guestbook:post-create-list')
+    success_message = 'Thanks for posting in the guestbook, %(name)s!'
 
     def get_context_data(self, **kwargs):
         # we override get_context_data here to add our list of posts to the
         # context so we can show the form and the list of posts on the one page
 
         post_list = get_valid_posts()
-        kwargs['post_list'] = post_list
+        paginator = Paginator(post_list, 5)
+        page = self.request.GET.get('page')
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+
+        kwargs['posts'] = posts
         kwargs['post_count'] = len(post_list)
         return super().get_context_data(**kwargs)
 
